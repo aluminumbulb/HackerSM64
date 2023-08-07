4c94fceb98534f1765374fc60ba0320946e9fb18
Funhouse Ghoul <adrianrichmiller@gmail.com>
2023-08-06 17:06:20 -0700

    Successfully Created Second Area, Working on Railing Gags

--- levels/castle_grounds/script.c
+++ levels/castle_grounds/script.c
@@ -52,7 +52,7 @@ const LevelScript level_castle_grounds_entry[] = {
 	/* Fast64 begin persistent block [level commands] */
 	/* Fast64 end persistent block [level commands] */
 
-	AREA(1, castle_grounds_area_1),
+	AREA(2, castle_grounds_area_2),
 		WARP_NODE(0x0A, LEVEL_CASTLE_GROUNDS, 0x01, 0x0A, WARP_NO_CHECKPOINT),
 		WARP_NODE(0xF0, LEVEL_CASTLE_GROUNDS, 0x01, 0x0A, WARP_NO_CHECKPOINT),
 		WARP_NODE(0xF1, LEVEL_CASTLE_GROUNDS, 0x01, 0x0A, WARP_NO_CHECKPOINT),
@@ -61,18 +61,30 @@ const LevelScript level_castle_grounds_entry[] = {
 		OBJECT(MODEL_BOO, 1600, 296, -941, 0, 0, 0, 0x00000000, bhvHeaveHo),
 		OBJECT(MODEL_BOO, -421, 287, -1845, 0, 0, 0, 0x00000000, bhvHeaveHo),
 		OBJECT(MODEL_BOO, -1899, 287, -12, 0, 0, 0, 0x00000000, bhvHeaveHo),
-		OBJECT(MODEL_NONE, 2216, 1177, -1537, 0, 0, 0, 0x000A0000, bhvSpinAirborneWarp),
-		MARIO_POS(0x01, 0, 2216, 1177, -1537),
-		TERRAIN(castle_grounds_area_1_collision),
-		MACRO_OBJECTS(castle_grounds_area_1_macro_objs),
+		TERRAIN(castle_grounds_area_2_collision),
+		MACRO_OBJECTS(castle_grounds_area_2_macro_objs),
 		STOP_MUSIC(0),
 		TERRAIN_TYPE(TERRAIN_GRASS),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
 	END_AREA(),
 
+	AREA(1, castle_grounds_area_1),
+		OBJECT(MODEL_BLACK_BOBOMB, 1783, 729, -178, 0, 0, 0, 0x00000000, bhvGoomba),
+		OBJECT(MODEL_BLACK_BOBOMB, 1783, 729, 82, 0, 0, 0, 0x00000000, bhvChuckya),
+		OBJECT(MODEL_BLACK_BOBOMB, 1783, 729, 334, 0, 0, 0, 0x00000000, bhvGoomba),
+		OBJECT(MODEL_NONE, 815, -43, -372, 0, 0, 0, 0x000A0000, bhvSpinAirborneWarp),
+		MARIO_POS(0x01, 0, 815, -43, -372),
+		TERRAIN(castle_grounds_area_1_collision),
+		MACRO_OBJECTS(castle_grounds_area_1_macro_objs),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_GRASS),
+		TERRAIN_TYPE(TERRAIN_GRASS),
+		/* Fast64 begin persistent block [area commands] */
+		/* Fast64 end persistent block [area commands] */
+	END_AREA(),
+
 	FREE_LEVEL_POOL(),
-	MARIO_POS(0x01, 0, 2216, 1177, -1537),
+	MARIO_POS(0x01, 0, 815, -43, -372),
 	CALL(0, lvl_init_or_update),
 	CALL_LOOP(1, lvl_init_or_update),
 	CLEAR_LEVEL(),
